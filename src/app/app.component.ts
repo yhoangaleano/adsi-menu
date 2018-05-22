576790bf1bda045ab874a737df24c9b919af7245
Yhoan Andres Galeano Urrea <yhoangaleano@gmail.com>
2018-05-21 20:37:56 -0500

    Login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,20 @@
 import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
+import { Platform, MenuController } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
-import { HomePage } from '../pages/home/home';
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = HomePage;
+  rootPage:any = 'LoginPage';
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  constructor(
+    platform: Platform,
+    statusBar: StatusBar,
+    splashScreen: SplashScreen,
+    menuCtrl : MenuController
+  ) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
@@ -18,5 +22,14 @@ export class MyApp {
       splashScreen.hide();
     });
   }
+
+  openPage(page: string){
+
+  }
+
+  closeMenu(){
+
+  }
+
 }
 
--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
 /**
- * Generated class for the HomePage page.
+ * Generated class for the LoginPage page.
  *
  * See https://ionicframework.com/docs/components/#navigation for more info on
  * Ionic pages and navigation.
@@ -10,16 +10,22 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
 @IonicPage()
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html',
+  selector: 'page-login',
+  templateUrl: 'login.html',
 })
-export class HomePage {
+export class LoginPage {
+
+  public imgLogo = './assets/imgs/logo.png'
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad HomePage');
+    console.log('ionViewDidLoad LoginPage');
+  }
+
+  public loguearse(){
+    this.navCtrl.setRoot('HomePage');
   }
 
 }
